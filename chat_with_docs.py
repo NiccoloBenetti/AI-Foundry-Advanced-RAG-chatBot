0267ee6b9a883fc5c6f6807f926f624fbf1e2ba8
Niccolo <160759878+NiccoloBenetti@users.noreply.github.com>
2025-04-08 18:06:29 +0200

    first commit

---
+++ chat_with_docs.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+from opentelemetry import trace
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from config import ASSET_PATH, get_logger, enable_telemetry
+from get_documents import get_documents
+from azure.ai.inference.prompts import PromptTemplate
+
+
+# initialize logging and tracing objects
+logger = get_logger(__name__)
+tracer = trace.get_tracer(__name__)
+
+# create a project client using environment variables loaded from the .env file
+project = AIProjectClient.from_connection_string(
+    conn_str=os.environ["AIPROJECT_CONNECTION_STRING"], credential=DefaultAzureCredential()
+)
+
+# create a chat client we can use for testing
+chat = project.inference.get_chat_completions_client()
+
+@tracer.start_as_current_span(name="chat_with_docs")
+def chat_with_docs(messages: list, context: dict = None) -> dict:
+    if context is None:
+        context = {}
+
+    documents = get_documents(messages, context)
+
+    # do a grounded chat call using the search results
+    grounded_chat_prompt = PromptTemplate.from_prompty(Path(ASSET_PATH) / "grounded_chat.prompty")
+
+    system_message = grounded_chat_prompt.create_messages(documents=documents, context=context)
+    response = chat.complete(
+        model=os.environ["CHAT_MODEL"],
+        messages=system_message + messages,
+        **grounded_chat_prompt.parameters,
+    )
+    #logger.info(f"Response: {response.choices[0].message}")
+
+    # Return a chat protocol compliant response
+    return {"message": response.choices[0].message, "context": context}
+
+if __name__ == "__main__":
+    print("RAG Chat. Write 'exit' to exit.\n")
+    history = []
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower().strip() == "exit":
+            print("Chat terminated!")
+            break
+
+        history.append({"role": "user", "content": user_input})
+        try:
+            response = chat_with_docs(messages=history)
+            ai_message = response["message"]["content"]
+            print(f"AI: {ai_message}\n")
+            history.append({"role": "assistant", "content": ai_message})
+        except Exception as e:
+            print(f"An error occurred: {e}")
